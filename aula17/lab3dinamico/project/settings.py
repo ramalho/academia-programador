eecaa125825def7c4847aebe7790c438301a0848
Luciano Ramalho <luciano@ramalho.org>
2012-08-03 13:17:27 -0300

    retirando referencia ao IBGE no exemplo

--- aula17/lab3dinamico/project/settings.py
+++ aula17/lab3dinamico/project/settings.py
@@ -1,4 +1,4 @@
-# Django settings for ibge_project project.
+# Django settings for project.
 
 import os
 
@@ -104,10 +104,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'ibge_project.urls'
+ROOT_URLCONF = 'project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'ibge_project.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
